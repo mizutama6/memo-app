2d960b9ffd23936cbc706e0cc55d16e1a499107b
mizutama6 <pajama.blog0+github20@gmail.com>
2021-02-20 19:36:51 +0900

    Headerコンポーネントの実装

--- memo-view/src/components/Header.vue
+++ memo-view/src/components/Header.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="header">
-    <h1>Memo App</h1>
-    
-    <div class="">
-      <button @click="$router.push({name: 'Home'})">Home</button>
-      <button @click="$router.push({name: 'Create'})">Create a memo</button>
-    </div>
-  </div>
+  <v-app-bar
+    app
+    absolute
+    color="cyan lighten-2"
+    elevate-on-scroll
+    scroll-target="#scrolling-techniques-7"
+  >
+    <v-toolbar-title @click="$router.push({ name: 'Home' })">
+      <v-btn class="text-h4" text color="#fff">Memo App</v-btn>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-card-actions>
+      <v-btn
+        @click="$router.push({ name: 'Create' })"
+        color="yellow lighten-2"
+        class="brown--text"
+      >
+        Create a new "memo"
+      </v-btn>
+    </v-card-actions>
+  </v-app-bar>
 </template>
