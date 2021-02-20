a0ad91904e80901c8c2a7fc413c61c3ae5f4db5c
mizutama6 <pajama.blog0+github20@gmail.com>
2021-02-20 13:33:34 +0900

    ページ作成、ルートの設定

---
+++ memo-view/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="header">
+    <h1>Memo App</h1>
+    
+    <div class="">
+      <button @click="$router.push({name: 'Home'})">Home</button>
+      <button @click="$router.push({name: 'Create'})">Create a memo</button>
+    </div>
+  </div>
+</template>
---
+++ memo-view/src/views/Create.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="create">
+    <form method="POST">
+      <div class="">
+        <label for="">タイトル</label>
+        <input type="text">
+      </div>
+
+      <div class="">
+        <label for="">説明</label>
+        <textarea></textarea>
+      </div>
+
+      <button type="submit">作成</button>
+    </form>
+  </div>
+</template>
