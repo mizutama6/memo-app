c70c80160698580eeb8779c1e5220b627b0569be
mizutama6 <pajama.blog0+github20@gmail.com>
2021-02-22 14:26:47 +0900

    コードの整理

--- memo-view/src/views/Detail.vue
+++ memo-view/src/views/Detail.vue
@@ -3,20 +3,34 @@
     <v-card>
       <v-card-text>
         <v-form readonly>
-          <v-text-field label="Title" :value="memo.title" outlined></v-text-field>
+          <v-text-field
+            label="Title"
+            :value="memo.title"
+            outlined
+          ></v-text-field>
 
-          <v-textarea label="Content" :value="memo.content" outlined auto-grow></v-textarea>
+          <v-textarea
+            label="Content"
+            :value="memo.content"
+            outlined
+            auto-grow
+          ></v-textarea>
 
-          <v-text-field label="Created at" :value="memo.created_at" outlined></v-text-field>
+          <v-text-field
+            label="Created at"
+            :value="memo.created_at"
+            outlined
+          ></v-text-field>
 
-          <v-btn
-            depressed
-            color="cyan lighten-2 white--text"
-            class="my-10"
-            @click="$router.push({ name: 'Edit' })"
-          >
-            edit
-          </v-btn>
+          <v-card-actions>
+            <v-btn
+              depressed
+              color="cyan lighten-2 white--text"
+              @click="$router.push({ name: 'Edit' })"
+            >
+              edit
+            </v-btn>
+          </v-card-actions>
         </v-form>
       </v-card-text>
     </v-card>
@@ -24,10 +38,10 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
   props: {
-    id: String,
+    id: Number,
   },
   data() {
     return {
@@ -35,9 +49,17 @@ export default {
     };
   },
   methods: {
+    /**
+     * 特定のmemoのデータを取得する
+     */
     async getData() {
-      const data = await axios.get(`https://memo-app-9826.herokuapp.com/api/memos/${this.id}`);
-      this.memo = data.data.data;
+      await axios.get(
+        `https://memo-app-9826.herokuapp.com/api/memos/${this.id}`
+      ).then(res => {
+        this.memo = res.data.data;
+      }).catch(err => {
+        console.log(err);
+      });
     },
   },
   mounted() {
@@ -45,8 +67,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-
-
-</style>
